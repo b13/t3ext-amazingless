29bdba1baaa9974718a3628e041323c79a822a9c
Benjamin Mack <benni@typo3.org>
2013-05-02 17:28:59 +0200

    [TASK] Raised version to 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'b:dreizehn, Stuttgart',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => 
 	array (
 		'depends' => 
